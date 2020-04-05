3c15bb128fce1ef4c0d761d67cf066be426aa562
Omar Shaaban <oma.shaaban@kay.sa>
2020-04-05 20:48:43 +0300

    sticky footer + corrected display of Ayah number

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+var arabicNumerals = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+var diacritics = ['ّ', 'َ', 'ً', 'ُ', 'ٌ', 'ِ', 'ٍ', 'ْ', 'ـ', 'ٰ'];
+var arabicCharacters = ['ء', 'ا', 'أ', 'إ', 'آ', 'ب', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'ة', 'و', 'ؤ', 'ي', 'ى'];
+var englishCharactersSmall = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var englishCharactersCapital = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 String.prototype.removeDirecritics = function () {
-    var diacritics = ['ّ', 'َ', 'ً', 'ُ', 'ٌ', 'ِ', 'ٍ', 'ْ', 'ـ', 'ٰ'];
+
     var newStr = '';
     for (var i = 0; i < this.length; i++) {
         var c = this[i];
@@ -12,6 +18,107 @@ String.prototype.removeDirecritics = function () {
 };
 
 
+String.prototype.getWords = function () {
+    var words = [];
+    var w = '';
+    var isArabic = false;
+
+    for (var i = 0; i < this.length; i++) {
+        var c = this.charAt(i);
+
+        if (arabicCharacters.indexOf(c) >= 0 || diacritics.indexOf(c) >= 0) {
+
+            if (!isArabic) {
+                if (w.length > 0) {
+                    words.push(w);
+                    w = '';
+                }
+
+                isArabic = true;
+            }
+
+            w += c;
+
+
+        } else if (englishCharactersCapital.indexOf(c) >= 0 || englishCharactersSmall.indexOf(c) >= 0) {
+
+            if (isArabic) {
+                if (w.length > 0) {
+                    words.push(w);
+                    w = '';
+                }
+
+                isArabic = false;
+            }
+
+            w += c;
+
+
+        } else {
+
+            if (w.length > 0) {
+                words.push(w);
+                w = '';
+            }
+        }
+
+    }
+
+    if (w.length > 0)
+        words.push(w);
+
+    return words;
+
+};
+
+String.prototype.wordCount = function () {
+    return this.getWords().length;
+};
+
+String.prototype.firstWords = function (count) {
+    var words = this.getWords();
+
+    if (count < words)
+        return words;
+
+    return words.slice(0, count);
+
+};
+
+
+String.prototype.reverse = function () {
+    var newStr = '';
+
+    for (var i = this.length - 1; i >= 0; i--) {
+        newStr += this[i];
+    }
+
+    return newStr;
+};
+
+
+String.prototype.toArabicHindiNumerals = function () {
+    var val = '';
+    for (var i = 0; i < this.length; i++) {
+        var num = parseInt(this[i]);
+
+        if (isNaN(num)) {
+            val += this[i];
+        } else {
+            val += arabicNumerals[num];
+        }
+    }
+
+    return val;
+};
+
+Number.prototype.toArabicHindiNumerals = function () {
+    var val = '';
+
+    var str = this.toString();
+
+    return str.toArabicHindiNumerals();
+};
 
 var quranHelper = {
     canvasElement: '#quranCanvas',
@@ -19,7 +126,15 @@ var quranHelper = {
     currentSurah: 0,
     currentAyah: 0,
 
+    options: {
+        gameMode: true,
+        minWordsInGame: 10
+    },
+
+
     data: [],
+
+    ayat: [],
 
     load: function () {
         var that = this;
@@ -27,6 +142,7 @@ var quranHelper = {
             .done(function (res) {
 
                 var data = [];
+                var ayat = [];
 
                 var lines = res.split('\n');
 
@@ -43,6 +159,11 @@ var quranHelper = {
 
                     if (isNaN(surah) || isNaN(ayah)) continue; // must be integers
 
+                    ayat.push({
+                        s: surah,
+                        a: ayah,
+                        text: text
+                    });
 
                     if (data.length < surah) {
                         data.push([]);
@@ -53,8 +174,16 @@ var quranHelper = {
                 }
 
                 that.data = data;
+                that.ayat = ayat;
 
-                that.nextAyah();
+                if (that.options.gameMode) {
+
+                    var rand = Math.floor(Math.random() * ayat.length);
+                    var sa = ayat[rand];
+                    that.goTo(sa.s, sa.a);
+                } else {
+                    that.nextAyah();
+                }
 
 
             });
@@ -139,8 +268,8 @@ var quranHelper = {
         this.goTo(surah, ayah);
     },
 
-    goTo: function (surah, ayah) {
-        if (isNaN(surah) || isNaN(ayah)) return; // must be integers
+    getAyahText: function (surah, ayah) {
+        if (isNaN(surah) || isNaN(ayah)) return null; // must be integers
 
         if (this.data.length > surah - 1 && this.data[surah - 1].length > ayah - 1) {
 
@@ -149,11 +278,35 @@ var quranHelper = {
                 text = text.substring(this.data[0][0].length).trim();
             }
 
-            $(this.canvasElement).text(text + ' ' + (ayah));
+            return text;
+        }
+
+        return null;
+    },
+    goTo: function (surah, ayah) {
+        var text = this.getAyahText(surah, ayah);
+
+        if (false && this.options.gameMode) {
+
+            var wordCount = text.wordCount();
+
+            if (wordCount > this.options.minWordsInGame) {
+
+            } else {
+
+            }
+
+
+        } else {
+
+            var ayahStr = ayah.toString().reverse();
+
+            $(this.canvasElement).text(text + ' ' + (ayahStr.toArabicHindiNumerals()));
 
             this.currentSurah = surah;
             this.currentAyah = ayah;
         }
+
     }
 };
 
